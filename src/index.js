554751516bf1708d258e4f74a21cd7a461d2e85e
arkaprovo <apb@live.in>
2022-04-07 21:59:27 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,52 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Keycloak from 'keycloak-js';
+
+
+
+//Get the keycloak configuration
+let keycloak = Keycloak('./resources/keycloak.json');
+
+//Initialization of the keycloak instance
+keycloak.init({ onLoad: 'login-required',redirectUri: 'https://intelligence.redhat.preprod.com' }).then((authenticated) => {
+
+    console.log(keycloak);
+    console.log(authenticated);
+    // console.log(getState().keycloakLogin);
+    if (!authenticated) {
+        window.location.reload();
+    } else {
+        console.info("Authenticated");
+    }
+
+    //React Render on authentication
+    ReactDOM.render(<App />, document.getElementById('root'));
+
+    //store authentication tokens in sessionStorage
+    sessionStorage.setItem('authentication', keycloak.token);
+    sessionStorage.setItem('refreshToken', keycloak.refreshToken);
+
+    //to regenerate token on expiry
+    setTimeout(() => {
+        keycloak.updateToken(70).success((refreshed) => {
+            if (refreshed) {
+                console.debug('Token refreshed' + refreshed);
+            } else {
+                console.warn('Token not refreshed, valid for '
+                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+            }
+        }).catch(() => {
+            console.error('Failed to refresh token');
+        });
+
+
+    }, 60000)
+
+}).catch(() => {
+    console.error("Authenticated Failed");
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
